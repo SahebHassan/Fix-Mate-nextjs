0f3f131e586b56fb7694a3d175112e39c6248867
SahebHassan <124240923+SahebHassan@users.noreply.github.com>
2024-10-03 13:00:47 +0530

    added auth

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">We&apos;re coming Nextjs</h1>
-      <h2 className="h2-bold">We&apos;re coming Nextjs</h2>
-      <h3 className="h3-bold">We&apos;re coming Nextjs</h3>
-    </div>
-  );
-}
